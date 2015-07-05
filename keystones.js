ccf771e3958cf2e816e6090070b56b0f71971283
Dominic Boulanger <dominic_boulanger@hotmail.com>
2015-07-05 11:08:55 -0400

    Added tooltip support for keystones

---
+++ keystones.js
@@ -0,0 +1,133 @@
+var keystones = [
+	{
+		"id": 0,
+		"name": "Acrobatics", 
+		"description": "30% Chance to Dodge Attacks\n50% less Armour and Energy Shield\n30% less Block chance"
+	},
+	
+	{
+		"id": 1,
+		"name": "Ancestral Bond", 
+		"description": "Can summon up to 1 additional totem\nYou can't deal Damage with your Skills yourself"
+	},
+	
+	{
+		"id": 2,
+		"name": "Avatar of Fire",
+	    "description": "50% of Physical Damage Converted to Fire Damage\n50% of Lightning Damage Converted to Fire Damage\n50% of Cold Damage Converted to Fire Damage\nDeal no Non-Fire Damage"
+	},
+	
+	{
+		"id": 3,
+		"name": "Blood Magic", 
+		"description": "Removes all mana\nSpend Life instead of Mana for Skills"
+	},
+	
+	{
+		"id": 4,
+		"name": "Chaos Inoculation", 
+		"description": "Maximum Life becomes 1\nImmune to Chaos Damage"
+	},
+	
+	{
+		"id": 5,
+		"name": "Conduit", 
+		"description": "Share Endurance, Frenzy and Power Charges with nearby party members"
+	},
+	
+	{
+		"id": 6,
+		"name": "Eldritch Battery", 
+		"description": "Converts all Energy Shield to Mana"
+	},
+	
+	{
+		"id": 7,
+		"name": "Elemental Equilibrium", 
+		"description": "Enemies you hit with Elemental Damage temporarily get +25% Resistances to those Elements and −50% Resistances to other Elements"
+	},
+	
+	{
+		"id": 8,
+		"name": "Ghost Reaver", 
+		"description": "Life Leech applies to Energy Shield instead of Life"
+	},
+	
+	{
+		"id": 9,
+		"name": "Iron Grip", 
+		"description": "The increase to Physical Damage from Strength applies to Projectile Attacks as well as Melee Attacks"
+	},
+	
+	{
+		"id": 10,
+		"name": "Iron Reflexes", 
+		"description": "Converts all Evasion Rating to Armour\nDexterity provides no bonus to Evasion Rating"
+	},
+	
+	{
+		"id": 11,
+		"name": "Mind Over Matter",
+		 "description": "When Hit, 30% of Damage is taken from Mana before Life"
+	},
+	
+	{
+		"id": 12,
+		"name": "Minion Instability",
+		"description": "Minions explode when reduced to low life, dealing 33% of their maximum life as fire damage to surrounding enemies"
+	},
+	
+	{
+		"id": 13,
+		"name": "Necromantic Aegis", 
+		"description": "All bonuses from an equipped Shield apply to your Minions instead of you"
+	},
+	
+	{
+		"id": 14,
+		"name": "Ondar's Guile", 
+		"description": "Doubles chance to Evade Projectile Attacks"
+	},
+	
+	{
+		"id": 15,
+		"name": "Pain Attunement", 
+		"description": "30% more Spell Damage when on Low Life"
+	},
+	
+	{
+		"id": 16,
+		"name": "Phase Acrobatics", 
+		"description": "30% Chance to Dodge Spell Damage"
+	},
+	
+	{
+		"id": 17,
+		"name": "Point Blank", 
+		"description": "Projectile Attacks deal up to 50% more Damage to targets at the start of their movement, dealing less Damage to targets as the projectile travels further"
+	},
+	
+	{
+		"id": 18,
+		"name": "Resolute Technique", 
+		"description": "Your hits can't be Evaded\nNever deal Critical Strikes"
+	},
+	
+	{
+		"id": 19,
+		"name": "Unwavering Stance", 
+		"description": "Cannot Evade enemy Attacks\nCannot be Stunned"
+	},
+	
+	{
+		"id": 20,
+		"name": "Vaal Pact", 
+		"description": "Life Leech applies instantly at 40% effectiveness\nLife Regeneration has no effect"
+	},
+	
+	{
+		"id": 21,
+		"name": "Zealot's Oath", 
+		"description": "Life Regeneration applies to Energy Shield instead of Life"
+	}
+];
